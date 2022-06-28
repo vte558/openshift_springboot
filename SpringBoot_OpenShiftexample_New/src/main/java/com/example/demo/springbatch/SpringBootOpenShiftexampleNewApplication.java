63f9362a87924759c83bdffa467469c6646148b9
91991 <91991@host.docker.internal>
2022-06-28 13:49:26 +0530

    test

---
+++ SpringBoot_OpenShiftexample_New/src/main/java/com/example/demo/springbatch/SpringBootOpenShiftexampleNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.springbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOpenShiftexampleNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOpenShiftexampleNewApplication.class, args);
+	}
+
+}
